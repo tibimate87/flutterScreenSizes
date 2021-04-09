a7b41d55f5738a61d52ea62faabde4158bae0444
tibimate87 <tibormate87@gmail.com>
2021-04-09 19:44:00 +0200

    add missing static

--- lib/screensizes.dart
+++ lib/screensizes.dart
@@ -58,15 +58,15 @@ class ScreenSizes {
     return textPainter.size;
   }
 
-  bool isSmallScreen(BuildContext context) {
+  static bool isSmallScreen(BuildContext context) {
     return MediaQuery.of(context).size.width < 800;
   }
 
-  bool isLargeScreen(BuildContext context) {
+  static bool isLargeScreen(BuildContext context) {
     return MediaQuery.of(context).size.width > 1200;
   }
 
-  bool isMediumScreen(BuildContext context) {
+  static bool isMediumScreen(BuildContext context) {
     return MediaQuery.of(context).size.width >= 800 &&
         MediaQuery.of(context).size.width <= 1200;
   }
